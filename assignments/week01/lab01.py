fcfbe3a481761bb1c4e6b6acdd1baa9d1b3c07cc
Drevyar <Kraivich.sa@.ku.th>
2025-07-08 14:51:38 +0700

    .

---
+++ assignments/week01/lab01.py
@@ -0,0 +1,15 @@
+print("=== Personal Profile Creator ===")
+
+name = input('What is your name :')
+age = int(input('How old are you? :'))
+email = input('Your Email :')
+phonenum = int(input('What is your phone number :'))
+favhob = input('What is your favorite hobby :')
+
+# Write your solution here:
+print("=== Your Personal Profile  ===")
+print(f'Hello, {name}')
+print(f'Age : {age}')
+print(f'Email : {email}')
+print(f'Phone number : {phonenum}')
+print(f'Your favorite hobby is : {favhob}')
