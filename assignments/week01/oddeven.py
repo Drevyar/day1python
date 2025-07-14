28c044eabd57b09232207c41e05606d9d996b92a
Drevyar <Kraivich.sa@ku.th>
2025-07-14 21:55:05 +0700

    .

--- assignments/week01/oddeven.py
+++ assignments/week01/oddeven.py
@@ -1,4 +1,5 @@
 
+num = []
 odd = []
 even = []
 
@@ -11,7 +12,17 @@ while True:
             continue
         
         for i in range(1, x1 + 1):
-            number = int(input(f'Number {i} : '))
+            while True:
+                try:
+                    number = int(input(f'Number {i} : '))
+                    if number < 0:
+                        print('number must more than 0.')
+                        continue
+                    break
+                except ValueError:
+                    print('Please enter a valid number.')
+                    continue
+            num.append(number)
             if number % 2 == 0:
                 even.append(number)
             else:
@@ -19,8 +30,10 @@ while True:
                 
         print(f'Even numbers : {even}')
         print(f'Odd numbers : {odd}')
+        print(f'Total numbers : {num}')
         print(f"Sum of even numbers: {sum(even)}")
         print(f"Sum of odd numbers: {sum(odd)}")
+        print(f'Total sum of number : {sum(num)}')
         break
             
     except ValueError:
